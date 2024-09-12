7a41f2b88d80c5d6b23e2fcabb10ae5d9a903960
Ingrid Solis <59096232+Joselota@users.noreply.github.com>
2024-09-11 21:15:13 -0300

    Actualizacion

--- ScriptSecundarios/Campos/CargaCampos5.py
+++ ScriptSecundarios/Campos/CargaCampos5.py
@@ -426,7 +426,6 @@ def main():
     bdg_cursor.close()
     print("fin cierre de cursores y bases")
     envio_mail("Fin proceso de Cargar Tablas en Campos 4/9")
-    exit(1)
 
 if __name__ == "__main__":
     main()
